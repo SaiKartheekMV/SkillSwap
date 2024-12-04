c4949c73b61722f51aec5830758994eecabd2ad3
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2024-12-04 21:00:08 +0530

    Adding new Styles and buttons for Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import "./Login.css";
 import gimg from "../assets/google-icon.svg";
+import logo from "../../public/SSlogo.png";
 
 const Login = () => {
   return (
@@ -7,15 +8,17 @@ const Login = () => {
       <div className="container">
         <div className="right-container">Right Side container</div>
         <div className="left-container">
-          <img src="" alt="icon" />
+          <img src={logo} alt="icon" className="logo" />
           <h2>Skill Swap</h2>
           <div className="inputBox">
             <input type="email" required />
             <span>Email</span>
           </div>
+          <span className="continue"><p>Continue</p></span>
           <h3 className="or">OR</h3>
           <div className="googlelogin">
-          <img src={gimg} className="gimg" alt="" />
+          <img src={gimg} className="gimg" alt="googleicon" /> 
+          <p>Continue With Google</p>
           </div>
         </div>
       </div>
